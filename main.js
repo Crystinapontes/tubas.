d570429a4f95a947876d274134cefda161277de2
Crystinapontes <146957310+Crystinapontes@users.noreply.github.com>
2024-10-31 12:04:13 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function criarCartao(pergunta, resposta ){
     let conteudo = document.getElementById("conteudo")
+   let cartao = document.createElement('article')
+   cartao.elasslit = 'cartao'
 
 
+   
 
     criarCartao(1,2)
 
